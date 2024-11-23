1d77987061fe86b60b67c4028fff0cbcbd9f3b4e
excellent <flarpzz08@gmail.com>
2024-11-23 20:57:47 +0100

    small layout fixes

--- src/components/NoProjectSelected.jsx
+++ src/components/NoProjectSelected.jsx
@@ -1,6 +1,6 @@
 import NoProjectsImage from "../assets/no-projects.png";
 
-export default function NoProjectSelected({onStartAddProject}) {
+export default function NoProjectSelected({ onStartAddProject }) {
   return (
     <div className="m-auto flex flex-col items-center">
       <img
@@ -8,9 +8,20 @@ export default function NoProjectSelected({onStartAddProject}) {
         className="md:size-24 size-20 mb-5"
         alt="Empty notes abstract image"
       />
-      <h2 className="font-bold text-2xl md:text-3xl md:mb-5 mb-2 text-stone-600">No Project Selected</h2>
-      <h3 className="font-semibold md:text-xl text-stone-500 md:mb-10 mb-5">Select a project or get started with a new one</h3>
-      <button onClick={onStartAddProject} className="md:px-5 md:py-3 px-3 py-2 rounded-md text-stone-400 bg-stone-700 font-semibold md:text-xl hover:bg-stone-600">Create new project</button>
+      <div class="text-5xl font-extrabold mb-3">
+        <span class="bg-clip-text text-transparent bg-gradient-to-r from-amber-950 to-yellow-500">
+          No Project Selected
+        </span>
+      </div>
+      <h3 className="font-semibold md:text-xl text-stone-500 md:mb-10 mb-5">
+        Select a project or get started with a new one
+      </h3>
+      <button
+        onClick={onStartAddProject}
+        className="md:px-5 md:py-3 px-3 py-2 rounded-md text-stone-400 bg-stone-700 font-semibold md:text-xl hover:bg-stone-600"
+      >
+        Create new project
+      </button>
     </div>
   );
 }
